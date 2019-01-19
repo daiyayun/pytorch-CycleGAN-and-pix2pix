77c036d03f33ca77a7ad423de55c39f6ce86caaa
daiyayun <dyy06148@163.com>
2019-01-19 23:55:09 +0100

    Update evaluate.py

--- scripts/eval_cityscapes/evaluate.py
+++ scripts/eval_cityscapes/evaluate.py
@@ -8,12 +8,15 @@ from util import segrun, fast_hist, get_scores
 from cityscapes import cityscapes
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--cityscapes_dir", type=str, required=True, help="Path to the original cityscapes dataset")
+# testB
+parser.add_argument("--label_dir", type=str, required=True, help="Path to the ground truth label")
+# prediction labels of testA
+parser.add_argument("--num_test", type=int, default=500, help="number of images to evaluate")
 parser.add_argument("--result_dir", type=str, required=True, help="Path to the generated images to be evaluated")
 parser.add_argument("--output_dir", type=str, required=True, help="Where to save the evaluation results")
 parser.add_argument("--caffemodel_dir", type=str, default='./scripts/eval_cityscapes/caffemodel/', help="Where the FCN-8s caffemodel stored")
 parser.add_argument("--gpu_id", type=int, default=0, help="Which gpu id to use")
-parser.add_argument("--split", type=str, default='val', help="Data split to be evaluated")
+# parser.add_argument("--split", type=str, default='test', help="Data split to be evaluated")
 parser.add_argument("--save_output_images", type=int, default=0, help="Whether to save the FCN output images")
 args = parser.parse_args()
 
@@ -25,9 +28,9 @@ def main():
         output_image_dir = args.output_dir + 'image_outputs/'
         if not os.path.isdir(output_image_dir):
             os.makedirs(output_image_dir)
-    CS = cityscapes(args.cityscapes_dir)
+    CS = cityscapes(args.label_dir)
     n_cl = len(CS.classes)
-    label_frames = CS.list_label_frames(args.split)
+    # label_frames = CS.list_label_frames(args.split)
     caffe.set_device(args.gpu_id)
     caffe.set_mode_gpu()
     net = caffe.Net(args.caffemodel_dir + '/deploy.prototxt',
@@ -35,13 +38,14 @@ def main():
                     caffe.TEST)
 
     hist_perframe = np.zeros((n_cl, n_cl))
-    for i, idx in enumerate(label_frames):
+    for idx in range(args.num_test):
         if i % 10 == 0:
-            print('Evaluating: %d/%d' % (i, len(label_frames)))
-        city = idx.split('_')[0]
+            print('Evaluating: %d/%d' % (i, args.num_test))
+        # city = idx.split('_')[0]
         # idx is city_shot_frame
-        label = CS.load_label(args.split, city, idx)
-        im_file = args.result_dir + '/' + idx + '_leftImg8bit.png'
+        # label = CS.load_label(args.split, city, idx)
+        label = CS.load_label(idx)
+        im_file = args.result_dir + '/' + idx + '_B_fake_B.jpg'
         im = np.array(Image.open(im_file))
         # im = scipy.misc.imresize(im, (256, 256))
         im = scipy.misc.imresize(im, (label.shape[1], label.shape[2]))
@@ -50,9 +54,9 @@ def main():
         if args.save_output_images > 0:
             label_im = CS.palette(label)
             pred_im = CS.palette(out)
-            scipy.misc.imsave(output_image_dir + '/' + str(i) + '_pred.jpg', pred_im)
-            scipy.misc.imsave(output_image_dir + '/' + str(i) + '_gt.jpg', label_im)
-            scipy.misc.imsave(output_image_dir + '/' + str(i) + '_input.jpg', im)
+            scipy.misc.imsave(output_image_dir + '/' + str(idx) + '_pred.jpg', pred_im)
+            scipy.misc.imsave(output_image_dir + '/' + str(idx) + '_gt.jpg', label_im)
+            scipy.misc.imsave(output_image_dir + '/' + str(idx) + '_input.jpg', im)
 
     mean_pixel_acc, mean_class_acc, mean_class_iou, per_class_acc, per_class_iou = get_scores(hist_perframe)
     with open(args.output_dir + '/evaluation_results.txt', 'w') as f:
